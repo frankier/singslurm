3eaa4a29832bd7b88dc906ebdd5023e4584bb677
Frankie Robertson <frankie@robertson.name>
2020-12-04 10:27:04 +0200

    Sleep for extra time for exit code to actuall be written if need be

--- singslurm/req_run.py
+++ singslurm/req_run.py
@@ -3,15 +3,24 @@ import time
 import uuid
 
 
+SLEEP_TIME = 0.2
+
+
 def check_output(args):
     from subprocess import CalledProcessError
     iden = uuid.uuid4().hex
     with open("/var/run/req_run/reqs", "a") as req_run:
         req_run.write(iden + " " + " ".join(args) + "\n")
     while not os.path.exists(f"/var/run/req_run/{iden}.code"):
-        time.sleep(0.2)
-    with open(f"/var/run/req_run/{iden}.code") as code_f:
-        exit_code = int(code_f.read().strip())
+        time.sleep(SLEEP_TIME)
+    exit_code_str = ""
+    while 1:
+        with open(f"/var/run/req_run/{iden}.code") as code_f:
+            exit_code_str = code_f.read().strip()
+        if exit_code_str:
+            break
+        time.sleep(SLEEP_TIME)
+    exit_code = int(exit_code_str)
     with open(f"/var/run/req_run/{iden}.stdout", "rb") as stdout_f:
         stdout = stdout_f.read()
     with open(f"/var/run/req_run/{iden}.stderr", "rb") as stderr_f:
